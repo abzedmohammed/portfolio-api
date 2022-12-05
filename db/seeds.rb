19170e5ef5e7f3f3365c9c7a98eacbf1538736be
abzedmohammed <ibraabzed@gmail.com>
2022-12-05 12:37:37 +0300

    updated seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 Project.create(title: "Nairo-BNB", description: "Nairobians are suffering from lack of accommodations and hotels. 
     Nairobnb is an app for booking a place to sleep and enjoy cutting edge technology while feeling right at home.", picture: "url", 
 url: "http://nairobnb-react.vercel.app/", tags: ["React", "Ruby", "Bootstrap"], date: "10/2022")
@@ -14,14 +6,14 @@ Project.create(title: "Buildsoko", description: "Search for building products an
 url: "", tags: ["Django", "MdBootstrap"], date: "11/2021")
 
 Project.create(title: "Gardenation", description: "Gardenation is a free web application that allows users to buy and sell flowers across Kenya", 
-picture: "url", url: "", tags: ["Dajngo", "MdBootstrap", "JS"], date: "02/2022")
+picture: "url", url: "", tags: ["Django", "MdBootstrap", "JS"], date: "02/2022")
 
 Project.create(title: "Beach Resort", description: "Reach beach resort displays available rooms and its features and price. Rooms can also be filtered by size, price and type.", 
 picture: "url", url: "https://react2021-beach-resort.netlify.app/", tags: ["React", " Contentful"], date: "04/2021")
 
 Project.create(title: "Insta-Clone", description: "Instagram clone application", picture: "url", url: "",
-tags: ["Dajngo", "MdBootstrap"], date: "10/2020")
+tags: ["Django", "MdBootstrap", "JQuery"], date: "10/2020")
 
-Project.create(title: "Awards")
-Project.create(title: "Abzedizo-Moviz")
-Project.create(title: "News")
+Project.create(title: "Awards", description: "Rate applications developed by different programmers and organisations", picture: "url", url: "", 
+tags: ["Django", "Contentful"], date: "10/2020")
+
